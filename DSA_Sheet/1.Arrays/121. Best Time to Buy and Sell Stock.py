dc62c72d7296d34c52e0e5b2d6545f89adb5288e
bavitha01 <bavithareddy08@gmail.com>
2025-01-07 19:28:33 +0530

    Updated Array problems

---
+++ DSA_Sheet/1.Arrays/121. Best Time to Buy and Sell Stock.py
@@ -0,0 +1,24 @@
+from prompt_toolkit.key_binding.bindings.named_commands import print_last_kbd_macro
+
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if not prices:
+            return 0
+
+        min_price = float('inf')
+        max_profit = 0
+
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            elif price - min_price > max_profit:
+                max_profit = price - min_price
+
+        return max_profit
+sol= Solution()
+print(sol.maxProfit([3,1,2,5,8]))
